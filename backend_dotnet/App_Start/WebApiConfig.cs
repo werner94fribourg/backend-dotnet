e5be7bac6414466bc66dae9afc3930033776a394
werner <werner@varo-azure-clou>
2022-12-05 07:01:24 +0000

    Add project files.

---
+++ backend_dotnet/App_Start/WebApiConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace backend_dotnet.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.EnableCors();
+            config.Routes.MapHttpRoute(
+                name: "DefaultAPi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+        }
+    }
+}
---
+++ backend_dotnet/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace backend_dotnet.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
